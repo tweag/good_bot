30759a555c20f04ab6f7fc2235101bfdce3408c6
Ryan Hinkel <ryanhinkel@gmail.com>
2020-07-21 00:07:45 -0400

    Expect all arguments in the tests

--- bots/guess_bot.test.js
+++ bots/guess_bot.test.js
@@ -12,17 +12,15 @@ test('Handles reward message properly', () => {
   const bot = new TestBot()
 
   bot._handleMessage({ text })
-  expect(bot.handleReward.mock.calls[0][0]).toBe('10');
-  expect(bot.handleReward.mock.calls[0][1]).toBe('jump');
-  expect(bot.handleReward.mock.calls[0][2]).toBe('90');
-  expect(bot.handleReward.mock.calls[0][3]).toBe('100');
+  expect(bot.handleReward).toBeCalledWith('10', 'jump', '90', '100');
 })
 
 test('Handles begin message properly', () => {
-  const text = `<@TESTID> Are you ready? begin jump, run, fly`
-  const expected = ['jump', 'run', 'fly']
   const bot = new TestBot()
+  const text = `<@TESTID> Are you ready? begin jump, run, fly`
+  const user = 'GODBOT'
+  const expectedSpace = ['jump', 'run', 'fly']
 
-  bot._handleMessage({ text })
-  expect(bot.handleExplorationSpace.mock.calls[0][0]).toStrictEqual(expected);
+  bot._handleMessage({ text, user })
+  expect(bot.handleExplorationSpace).toBeCalledWith(expectedSpace, user);
 })
