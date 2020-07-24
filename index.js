ed80d66f18fdf10eeb35275d4fa659cc02ec5d92
Ryan Hinkel <ryanhinkel@gmail.com>
2020-07-24 12:25:55 -0400

    Stub out game over in client, and check remaining before guessing

--- index.js
+++ index.js
@@ -25,8 +25,14 @@ class MyBot extends GuessBot {
 
     // This is where your guess logic goes. The idea is to maximize your total points.
     // Currently, I'm just iterating over the whole action space.
-    const nextGuessIndex = (this.space.indexOf(guess) + 1) % this.space.length
-    this.makeGuess(this.space[nextGuessIndex])
+    if (remaining > 0) {
+      const nextGuessIndex = (this.space.indexOf(guess) + 1) % this.space.length
+      this.makeGuess(this.space[nextGuessIndex])
+    }
+  }
+
+  handleGameOver ({ totalScore }, banditBotId) {
+    this.send(`I scored a total of ${totalScore} points.`)
   }
 
   makeGuess (guess) {
